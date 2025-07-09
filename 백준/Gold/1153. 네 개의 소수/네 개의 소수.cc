d16d14872049794788a53364cdd30b60a3b5af24
trymimi <152114754+trymimi@users.noreply.github.com>
2025-07-09 11:53:38 +0900

    [Gold III] Title: 네 개의 소수, Time: 4 ms, Memory: 2996 KB -BaekjoonHub

--- 백준/Gold/1153. 네 개의 소수/네 개의 소수.cc
+++ 백준/Gold/1153. 네 개의 소수/네 개의 소수.cc
@@ -1,10 +1,27 @@
 #include <iostream>
-#include <set>
-#include <string>
-#include <algorithm>
-#include <vector>
-#include <queue>
 using namespace std;
+
+#define MAX 1000001
+bool isNotPrime[MAX];
+void eratos() {
+	isNotPrime[0] = isNotPrime[1] = true;
+	for (int i = 2; i * i < MAX; i++) {
+		if (!isNotPrime[i]) {
+			for (int j = i + i; j < MAX; j += i) {
+				isNotPrime[j] = true;
+			}
+		}
+	}
+}
+
+void goldbach(int n) {
+	for (int i = 2; i <= n / 2; i++) {
+		if (!isNotPrime[i] && !isNotPrime[n - i]) {
+			cout << i << ' ' << n - i;
+			break;
+		}
+	}
+}
 
 int main() {
 	ios_base::sync_with_stdio(false);
@@ -14,58 +31,26 @@ int main() {
 	int n;
 	cin >> n;
 
-	vector<int> sosu(n, 1);
-	sosu[1] = 0;
-	for (int i = 2; i < n; i++) {
-		if (sosu[i]) {
-			for (int j = i + i; j < n; j += i) {
-				sosu[j] = 0;
-			}
-		}
-	}
+	// 에라토스테네스의 체
+	eratos();
 
+	// 8 미만은 불가능
 	if (n < 8) {
 		cout << -1;
 		return 0;
 	}
 
-	if (n == 8) {
-		cout << 2 << ' ' << 2 << ' ' << 2 << ' ' << 2;
-		return 0;
-	}
-
+	// 홀수일 때
 	if (n % 2 == 1) {
-		n -= 3;
-		if (n < 6) {
-			cout << -1;
-			return 0;
-		}
-		if (n == 6) {
-			cout << 2 << ' ' << 2 << ' ' << 2 << ' ' << 3;
-			return 0;
-		}
-		int tmp;
-		for (int i = 2; i <= n && sosu[i]; i++) {
-			tmp = n - i;
-			for (int j = 2; j * 2 <= tmp; j++) {
-				if (sosu[j] && sosu[tmp - j]) {
-					cout << 3 << ' ' << i << ' ' << j << ' ' << tmp - j;
-					return 0;
-				}
-			}
-		}
-
+		n -= 5;	// 3 빼면 짝수. 2 빼도 골드바흐 성립
+		cout << 2 << ' ' << 3 << ' ';
 	}
+	// 짝수일 때
 	else {
-		n -= 4;
-		for (int i = 2; i <= n / 2; i++) {
-			if (sosu[i] && sosu[n - i]) {
-				cout << 2 << ' ' << 2 << ' ' << i << ' ' << n - i;
-				return 0;
-			}
-		}
+		n -= 4;	// 2를 두번 빼도 골드바흐 성립
+		cout << 2 << ' ' << 2 << ' ';
 	}
 
-	cout << -1;
+	goldbach(n);
 	return 0;
 }
