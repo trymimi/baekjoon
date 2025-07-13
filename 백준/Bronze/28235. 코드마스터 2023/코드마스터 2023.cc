9854651000b1e0033d47283741f4074b444a561c
trymimi <152114754+trymimi@users.noreply.github.com>
2025-07-14 01:01:16 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(0);
+
+    string s;
+    cin >> s;
+
+    if (s == "SONGDO") cout << "HIGHSCHOOL";
+    else if (s == "CODE") cout << "MASTER";
+    else if (s == "2023") cout << "0611";
+    else if (s == "ALGORITHM") cout << "CONTEST";
+}
