095815ef6ba945de2e4d80f59d41dc9ac132909f
trymimi <152114754+trymimi@users.noreply.github.com>
2025-08-01 11:02:42 +0900

    [Silver II] Title: 소가 길을 건너간 이유 5, Time: 0 ms, Memory: 2120 KB -BaekjoonHub

---
+++ 백준/Silver/14465. 소가 길을 건너간 이유 5/소가 길을 건너간 이유 5.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+bool blink[100001];
+
+int main()
+{
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(0);
+
+    int n, k, b; cin >> n >> k >> b;
+
+    while (b--) {
+        int temp; cin >> temp;
+        blink[temp] = true;
+    }
+
+    int cnt = 0;
+    for (int i = 1; i <= k; i++) if (blink[i]) cnt++;
+
+    int result = cnt;
+    for (int i = 1; i + k <= n; i++) {
+        if (blink[i]) cnt--;
+        if (blink[i + k]) cnt++;
+
+        result = min(result, cnt);
+    }
+
+    cout << result;
+}
