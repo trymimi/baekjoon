f1c5771503b067472cdc88ff1e35d0e142d4a586
trymimi <152114754+trymimi@users.noreply.github.com>
2025-08-11 15:09:48 +0900

    [Bronze V] Title: A+B - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cin.tie(0);
+	cout.tie(0);
+	ios_base::sync_with_stdio(0);
+
+	int a, b;
+	while (cin >> a >> b)
+		cout << a + b << '\n';
+}
