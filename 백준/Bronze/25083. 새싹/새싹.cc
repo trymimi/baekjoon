0f3100ecfb7a432f088ad9312b4bc0616be1a662
trymimi <152114754+trymimi@users.noreply.github.com>
2025-08-11 15:56:55 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cin.tie(0);
+	cout.tie(0);
+	ios_base::sync_with_stdio(0);
+
+	cout << "         ,r'\"7" << '\n';
+	cout << "r`-_   ,'  ,/" << '\n';
+	cout << " \\. \". L_r'" << '\n';
+	cout << "   `~\\/" << '\n';
+	cout << "      |" << '\n';
+	cout << "      |" << '\n';
+}
